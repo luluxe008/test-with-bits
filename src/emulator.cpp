65f663201b8f6e79835c5d155627b7e58011db61
luluxe008 <lucas.sevin08@gmail.com>
2021-10-30 09:22:56 +0000

    comment unfinish

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+/*
+ * _____________________
+ * | CURRENT INSTRUCTION ADRESS
+ * |___________________________
+ * | PROGRAM LENGTH
+ * |___________________________
+ * | PROGRAM INSTRUCTION
+ * |
+ * |___________________________
+ * | PROGRAM MEMORY
+ * |___________________
+ * 
+ */
 
 int main(){
 
